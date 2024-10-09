efc5a1ec33a1bb54f9f3e2241294c71e18366721
setiawanDedi05 <dssetiawan05@gmail.com>
2024-10-09 22:10:57 +0700

    add navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "@/public/logo.png";
+import { AuthModal } from "./AuthModal";
+
+export function Navbar() {
+  return (
+    <div className="flex py-5 items-center justify-between">
+      <Link href="/" className="flex items-center gap-2">
+        <Image src={Logo} alt="Logo" className="size-10" />
+        <h4 className="text-3xl font-semibold">
+          Remind<span className="text-primary">Event</span>
+        </h4>
+      </Link>
+      <AuthModal />
+    </div>
+  );
+}
