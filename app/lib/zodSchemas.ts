9e379629f9d9a39b9320b77f27984ccc3596696f
setiawanDedi05 <dssetiawan05@gmail.com>
2024-10-10 20:14:49 +0700

    add setting schema and event schema

--- app/lib/zodSchemas.ts
+++ app/lib/zodSchemas.ts
@@ -65,3 +65,21 @@ export function onboardingSchemaValidation(options?: {
       .max(150, { message: "Fullname must be less than 150 character" }),
   });
 }
+
+export const settingsSchema = z.object({
+  fullname: z
+    .string()
+    .min(3, {
+      message: "Fullname must be more than 3 character",
+    })
+    .max(150, { message: "Fullname must be less than 150 character" }),
+  profileImage: z.string(),
+});
+
+export const eventTypeSchema = z.object({
+  title: z.string().min(3).max(150),
+  duration: z.number().min(15).max(60),
+  url: z.string().min(3).max(150),
+  description: z.string().min(3).max(300),
+  videoCallSoftware: z.string().min(3),
+});
