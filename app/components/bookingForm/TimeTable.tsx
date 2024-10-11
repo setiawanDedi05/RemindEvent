b60294ca1ac4b32d78e6de1a67a2a452e726aeb0
setiawanDedi05 <dssetiawan05@gmail.com>
2024-10-11 19:50:07 +0700

    wip time availability feature in booking page

---
+++ app/components/bookingForm/TimeTable.tsx
@@ -0,0 +1,65 @@
+import prisma from "@/app/lib/db";
+import { nylas } from "@/app/lib/nylas";
+import { Prisma } from "@prisma/client";
+import { format } from "date-fns";
+interface iAppProps {
+  selectedDate: Date;
+  username: string;
+}
+
+async function getData(username: string, selectedDate: Date) {
+  const currentDay = format(selectedDate, "EEEE");
+
+  const startOfDay = new Date(selectedDate);
+  startOfDay.setHours(0, 0, 0, 0);
+  const endOfDay = new Date(selectedDate);
+  endOfDay.setHours(23, 59, 59, 999);
+  const data = await prisma.availability.findFirst({
+    where: {
+      day: currentDay as Prisma.EnumDayFilter,
+      User: {
+        userName: username,
+      },
+    },
+    select: {
+      fromTime: true,
+      tillTime: true,
+      id: true,
+      User: {
+        select: {
+          grantEmail: true,
+          grantId: true,
+        },
+      },
+    },
+  });
+
+  const nylasCalendarData = await nylas.calendars.getFreeBusy({
+    identifier: data?.User?.grantId as string,
+    requestBody: {
+      startTime: Math.floor(startOfDay.getTime() / 1000),
+      endTime: Math.floor(endOfDay.getTime() / 1000),
+      emails: [data?.User?.grantEmail as string],
+    },
+  });
+
+  return {
+    data,
+    nylasCalendarData,
+  };
+}
+
+export async function TimeTable({ selectedDate, username }: iAppProps) {
+  const { data, nylasCalendarData } = await getData(username, selectedDate);
+  
+  return (
+    <div>
+      <p className="text-base font-semibold">
+        {format(selectedDate, "EEE")}{" "}
+        <span className="text-sm text-muted-foreground">
+          {format(selectedDate, "MMM. d")}
+        </span>
+      </p>
+    </div>
+  );
+}
