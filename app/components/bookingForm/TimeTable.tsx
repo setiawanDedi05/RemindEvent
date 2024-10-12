afa3a5207c410236315b786df384f4beef5e0501
setiawanDedi05 <dssetiawan05@gmail.com>
2024-10-12 21:32:22 +0700

    finishing booking page

--- app/components/bookingForm/TimeTable.tsx
+++ app/components/bookingForm/TimeTable.tsx
@@ -1,10 +1,21 @@
 import prisma from "@/app/lib/db";
 import { nylas } from "@/app/lib/nylas";
+import { Button } from "@/components/ui/button";
 import { Prisma } from "@prisma/client";
-import { format } from "date-fns";
+import {
+  addMinutes,
+  format,
+  fromUnixTime,
+  isAfter,
+  isBefore,
+  parse,
+} from "date-fns";
+import Link from "next/link";
+import { GetFreeBusyRequest, GetFreeBusyResponse, NylasResponse } from "nylas";
 interface iAppProps {
   selectedDate: Date;
   username: string;
+  duration: number;
 }
 
 async function getData(username: string, selectedDate: Date) {
@@ -49,9 +60,77 @@ async function getData(username: string, selectedDate: Date) {
   };
 }
 
-export async function TimeTable({ selectedDate, username }: iAppProps) {
+function calculateAvailableTimeSlots(
+  date: string,
+  dbAvailability: {
+    fromTime: string | undefined;
+    tillTime: string | undefined;
+  },
+  nylasData: NylasResponse<GetFreeBusyResponse[]>,
+  duration: number
+) {
+  const now = new Date();
+
+  const availableFrom = parse(
+    `${date} ${dbAvailability.fromTime}`,
+    "yyyy-MM-dd HH:mm",
+    new Date()
+  );
+
+  const availabilityTill = parse(
+    `${date} ${dbAvailability.tillTime}`,
+    "yyyy-MM-dd HH:mm",
+    new Date()
+  );
+
+  const busySlots = nylasData.data[0].timeSlots.map((slot) => ({
+    start: fromUnixTime(slot.startTime),
+    end: fromUnixTime(slot.endTime),
+  }));
+
+  const allSlots = [];
+  let currentSlot = availableFrom;
+
+  while (isBefore(currentSlot, availabilityTill)) {
+    allSlots.push(currentSlot);
+    currentSlot = addMinutes(currentSlot, duration);
+  }
+
+  const freeSlots = allSlots.filter((slot) => {
+    const slotEnd = addMinutes(slot, duration);
+
+    return (
+      isAfter(slot, now) &&
+      !busySlots.some(
+        (busy: { start: any; end: any }) =>
+          (!isBefore(slot, busy.start) && isBefore(slot, busy.end)) ||
+          (isAfter(slotEnd, busy.start) && !isAfter(slotEnd, busy.end)) ||
+          (isBefore(slot, busy.start) && isAfter(slotEnd, busy.end))
+      )
+    );
+  });
+
+  return freeSlots.map((slot) => format(slot, "HH:mm"));
+}
+
+export async function TimeTable({
+  selectedDate,
+  username,
+  duration,
+}: iAppProps) {
   const { data, nylasCalendarData } = await getData(username, selectedDate);
-  
+
+  const formattedDate = format(selectedDate, "yyyy-MM-dd");
+  const dbAvailability = {
+    fromTime: data?.fromTime,
+    tillTime: data?.tillTime,
+  };
+  const availableSlots = calculateAvailableTimeSlots(
+    formattedDate,
+    dbAvailability,
+    nylasCalendarData,
+    duration
+  );
   return (
     <div>
       <p className="text-base font-semibold">
@@ -60,6 +139,23 @@ export async function TimeTable({ selectedDate, username }: iAppProps) {
           {format(selectedDate, "MMM. d")}
         </span>
       </p>
+
+      <div className="mt-3 max-h-[350px] overflow-y-auto">
+        {availableSlots.length > 0 ? (
+          availableSlots.map((slot, index) => (
+            <Link
+              href={`?date=${format(selectedDate, "yyyy-MM-dd")}&time=${slot}`}
+              key={index}
+            >
+              <Button className="w-full mb-2" variant="outline">
+                {slot}
+              </Button>
+            </Link>
+          ))
+        ) : (
+          <p>No Available</p>
+        )}
+      </div>
     </div>
   );
 }
